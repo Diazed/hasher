4940dcb0e33e4de4ed3d56b32fbc5db6a1c19518
mmueller <maik.mueller@kisters.de>
2018-11-05 20:17:01 +0100

    Initial Commit

---
+++ src/main/java/de/maik/hasher/Hasher.java
@@ -0,0 +1,45 @@
+package de.maik.hasher;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.io.Console;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import org.apache.commons.codec.binary.Hex;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class Hasher {
+
+  public static void main(String[] args) {
+
+    Console con = System.console();
+    if (con == null) {
+      System.out.println("Run in Real Console.");
+    } else {
+      System.out.print("Password: ");
+      final String password = String.valueOf(con.readPassword());
+      if (password.length() == 0) {
+        System.out.println("Please provide the password");
+      } else {
+        MessageDigest messageDigest;
+        Security.addProvider(new BouncyCastleProvider());
+        try {
+          messageDigest = MessageDigest.getInstance("SHA-512", "BC");
+          byte[] digesta = messageDigest.digest(password.getBytes());
+
+          StringSelection selection = new StringSelection(String.valueOf(Hex.encodeHex(digesta)));
+          Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+          clipboard.setContents(selection, selection);
+          System.out.println("Copied hashed password into your clipboard.");
+        } catch (NoSuchAlgorithmException e) {
+          e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
